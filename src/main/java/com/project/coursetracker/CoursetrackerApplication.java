92dae9371fde1905855585d3871239ff25117ea4
randoelicor <Jose_Leonard_Rico@manulife.ca>
2023-05-21 16:39:07 +0800

    Added environment specific configuration files

--- src/main/java/com/project/coursetracker/CoursetrackerApplication.java
+++ src/main/java/com/project/coursetracker/CoursetrackerApplication.java
@@ -1,13 +1,40 @@
 package com.project.coursetracker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import javax.swing.*;
+import java.util.Properties;
 
 @SpringBootApplication
 public class CoursetrackerApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(CoursetrackerApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(CoursetrackerApplication.class, args);
+		Properties properties = new Properties();
+		properties.setProperty("spring.config.on-not-found", "ignore");
+
+//		SpringApplication application = new SpringApplication(CoursetrackerApplication.class);
+//
+//		application.setDefaultProperties(properties);
+//		application.run(args);
+
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(CoursetrackerApplication.class);
+
+		DbConfiguration dbConfiguration = applicationContext.getBean(DbConfiguration.class);
+
+		Environment env = applicationContext.getBean(Environment.class);
+
+		log.info("Timeout: " + env.getProperty("app.timeout"));
+
+		log.info(dbConfiguration.toString());
+
+//		SpringApplication.run(CoursetrackerApplication.class, args);
 	}
 
 }
