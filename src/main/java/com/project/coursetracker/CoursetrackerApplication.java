ec3f5b08d6332ebe9d934460fbf08585859cf56e
randoelicor <Jose_Leonard_Rico@manulife.ca>
2023-05-21 20:08:03 +0800

    Added CommandLineRunner

--- src/main/java/com/project/coursetracker/CoursetrackerApplication.java
+++ src/main/java/com/project/coursetracker/CoursetrackerApplication.java
@@ -3,10 +3,12 @@ package com.project.coursetracker;
 import com.project.coursetracker.configurationproperties.AppProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import javax.swing.*;
@@ -14,9 +16,10 @@ import java.util.Properties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+//public class CoursetrackerApplication implements CommandLineRunner {
 public class CoursetrackerApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(CoursetrackerApplication.class);
+	private static final Logger logger = LoggerFactory.getLogger(CoursetrackerApplication.class);
 
 	public static void main(String[] args) {
 		Properties properties = new Properties();
@@ -30,15 +33,30 @@ public class CoursetrackerApplication {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(CoursetrackerApplication.class);
 
 		DbConfiguration dbConfiguration = applicationContext.getBean(DbConfiguration.class);
-		log.info(dbConfiguration.toString());
+		logger.info(dbConfiguration.toString());
 
 		Environment env = applicationContext.getBean(Environment.class);
-		log.info("Timeout: " + env.getProperty("app.timeout"));
+		logger.info("Timeout: " + env.getProperty("app.timeout"));
 
 		AppService appService = applicationContext.getBean(AppService.class);
-		log.info(appService.getAppProperties().toString());
+		logger.info(appService.getAppProperties().toString());
 
 //		SpringApplication.run(CoursetrackerApplication.class, args);
+	}
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		logger.info("Application CommandLineRunner has executed...");
+//	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			logger.info("CommandLineRunner executed as a bean definition with " + args.length + " arguments");
+			for (int i = 0; i < args.length; i++) {
+				logger.info("Argument: " + args[i]);
+			}
+		};
 	}
 
 }
---
+++ src/main/java/com/project/coursetracker/commandline/MyCommandLineRunner.java
@@ -0,0 +1,20 @@
+package com.project.coursetracker.commandline;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Component
+public class MyCommandLineRunner implements CommandLineRunner {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("MyCommandLineRunner executed as a Spring Component");
+    }
+
+}
