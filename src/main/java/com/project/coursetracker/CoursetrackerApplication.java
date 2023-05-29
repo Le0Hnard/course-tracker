acf88bdc8a9f7e1de35c95c9222a6078de139871
randoelicor <Jose_Leonard_Rico@manulife.ca>
2023-05-29 14:22:45 +0800

    Added validation rules

--- src/main/java/com/project/coursetracker/CoursetrackerApplication.java
+++ src/main/java/com/project/coursetracker/CoursetrackerApplication.java
@@ -1,6 +1,8 @@
 package com.project.coursetracker;
 
 import com.project.coursetracker.configurationproperties.AppProperties;
+import com.project.coursetracker.model.Course;
+import com.project.coursetracker.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -12,12 +14,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import javax.swing.*;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.Properties;
+import java.util.Set;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-//public class CoursetrackerApplication implements CommandLineRunner {
-public class CoursetrackerApplication {
+public class CoursetrackerApplication implements CommandLineRunner {
+//public class CoursetrackerApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(CoursetrackerApplication.class);
 
@@ -44,10 +50,38 @@ public class CoursetrackerApplication {
 //		SpringApplication.run(CoursetrackerApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
+	@Override
+	public void run(String... args) throws Exception {
 //		logger.info("Application CommandLineRunner has executed...");
-//	}
+		Course course = new Course();
+		course.setId(1);
+		course.setRating(0);
+
+		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+		Set<ConstraintViolation<Course>> violations = validator.validate(course);
+		violations.forEach(courseConstraintViolation -> logger.error("A constraint violation has occurred. Violation details: [{}].", courseConstraintViolation));
+
+		User user1 = new User("sbip01", "sbip");
+		Set<ConstraintViolation<User>> userViolations = validator.validate(user1);
+		logger.error("Password for user1 do not adhere to the password policy");
+		userViolations.forEach(constraintViolation -> logger.error("Violation details: [{}].", constraintViolation.getMessage()));
+
+		User user2 = new User("sbip02", "Sbip01$4UDfg");
+		userViolations = validator.validate(user2);
+		if(userViolations.isEmpty()) {
+			logger.info("Password for user2 adhere to the password policy");
+		}
+
+		User user3 = new User("sbip03", "Sbip01$4UDfgggg");
+		userViolations = validator.validate(user3);
+		logger.error("Password for user3 violates maximum repetitive rule");
+		userViolations.forEach(constraintViolation -> logger.error("Violation details: [{}].", constraintViolation.getMessage()));
+
+		User user4 = new User("sbip04", "Sbip014UDfgggg");
+		userViolations = validator.validate(user4);
+		logger.error("Password for user4 violates special character rule");
+		userViolations.forEach(constraintViolation -> logger.error("Violation details: [{}].", constraintViolation.getMessage()));
+	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner() {
---
+++ src/main/java/com/project/coursetracker/model/User.java
@@ -0,0 +1,32 @@
+package com.project.coursetracker.model;
+
+import com.project.coursetracker.validation.Password;
+
+public class User {
+
+    private String userName;
+
+    @Password
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
